9433c53e8cff0a2a108b38d628e2206a34706909
Archontis Kostis <arxontisk02@gmail.com>
2023-04-28 12:01:28 +0300

    Update PriorityType.py

--- app/model/PriorityType.py
+++ app/model/PriorityType.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
-
+# This enum is currently not used in the application because I could not make it work with the front-end.
+# I think that it will be better if I find a way to make it work though
 class PriorityType(Enum):
     HIGH = 4
     MEDIUM = 3
