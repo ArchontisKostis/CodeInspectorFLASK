6713ae3d8d3e4fb809fd78a5c26001c53f24da3d
Archontis Kostis <arxontisk02@gmail.com>
2023-04-28 12:00:04 +0300

    removed unused imports

--- app/model/__init__.py
+++ app/model/__init__.py
@@ -1,6 +1,3 @@
-from app.model.PriorityType import PriorityType
-from app.model.RepoFile import RepoFile
-
 def is_not_already_added(file, list):
     for item in list:
         if file.filename == item.filename:
